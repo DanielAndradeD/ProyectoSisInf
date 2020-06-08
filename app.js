e544a30f2a4be6c12ac0297089512534be264247
DanielAndradeD <danielmetallica98@gmail.com>
2020-06-08 13:15:19 -0500

    Se agrego el archivo insertar.hbs, se modificó el index.js y app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/videojuegos', usersRouter);
 
 var mongoose = require('mongoose');
 var Videojuego = require('./models/videojuegos');
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Videojuegos' });
 });
 
+router.get('/agregar', function(req, res, next) {
+  res.render('insertar', { title: 'Alta de Videojuegos' });
+});
 module.exports = router;
