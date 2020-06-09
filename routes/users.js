461167eea8f39395a36bd0c6595a806eb0381cbb
Alejandro Cruz Hernández <alejandrocruz309@aragon.unam.mx>
2020-06-09 09:47:40 -0500

    Se añaden los metodos Delete y Patch

--- routes/users.js
+++ routes/users.js
@@ -6,13 +6,13 @@ var Videojuego = require('../models/videojuegos');
 
 router.get('/',(req, res, next)=>{
   Videojuego.find({},(err, datos)=>{
-    if (err)res.status(400).json({"mensaje":"No se encontro"})
+    if (err)res.status(400).json({"mensaje":"Error, no se encontro elemento"})
     res.status(200).json(datos);
   });
 });
 
 router.get('/:videojuegoId',(req, res, next)=>{
-  Auto.findOne({'_id':req.params.videojuegoId},(err, datos)=>{
+  Videojuego.findOne({'_id':req.params.videojuegoId},(err, datos)=>{
     if (datos == null) {
       res.status(404).json({"mensaje":"Elemento no encontrado"});
     } else {
@@ -41,6 +41,41 @@ router.post('/',(req, res, next)=>{
 
 router.post('/:videojuegoId',(req, res, next)=>{
   res.status(404).json({"mensaje":"Operacion no permitida"});
+});
+
+router.delete('/',(req, res, next)=>{
+  res.status(405).json({"mensaje":"Operacion no permitida"});
+});
+
+router.delete('/:videojuegoId',(req, res, next)=>{
+  Videojuego.findOneAndDelete({'_id':req.params.videojuegoId},(err, datos)=>{
+    if (err) {
+      res.status(404).json({"mensaje":"Elemento no encontrado"});
+    } else {
+      res.status(200).json(datos);
+    }
+  });
+});
+
+router.patch('/',(req, res, next)=>{
+  res.status(405).json({"mensaje":"Operacion no permitida"});
+});
+
+router.patch('/:videojuegoId',(req, res, next)=>{
+  Videojuego.findOneAndUpdate({'_id':req.params.videojuegoId}, {
+    nombre: req.body.nombre,
+    compania: req.body.compania,
+    clasificacion: req.body.clasificacion,
+    tipoJuego: req.body.tipoJuego,
+    numJugadores: req.body.numJugadores,
+    precio: req.body.precio
+  },(err, datos)=>{
+    if (err) {
+      res.status(404).json({"mensaje":"Elemento no encontrado"});
+    } else {
+      res.status(200).json(datos);
+    }
+  });
 });
 
 module.exports = router;
